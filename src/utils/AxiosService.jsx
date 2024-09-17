40aa23094afa3bb9340430c12c449249f81129c3
priya <rameshpriyait@gmail.com>
2024-09-17 18:00:20 +0530

    back end

--- src/utils/AxiosService.jsx
+++ src/utils/AxiosService.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const AxiosService = axios.create({
-    baseURL:"http://localhost:8000",
+    baseURL:"https://dback-end-1.onrender.com/",
     headers:{
         "Content-Type":"application/json",
     }
